c30343bfaece72695c3a15f47c0891303b6edcf5
Abhijit J. Theophilus <abhijit.theo@gmail.com>
2017-11-01 15:20:51 +0530

    Finished the PSO code.

--- cdhs_by_pso.py
+++ cdhs_by_pso.py
@@ -3,10 +3,12 @@
 import functools
 import numpy as np
 from exoplanets import exoplanets
-# from pso import Swarm
+from pso import Swarm
 
 # Required constants.
 SCALE = 20
+NPART = 10
+
 
 # Drop rows with missing values.
 exoplanets.dropna(how='any', inplace=True)
@@ -14,7 +16,6 @@ exoplanets.dropna(how='any', inplace=True)
 
 def penalty(pos, type_):
     """The penalty function for the Cobb Douglas function."""
-
     sum_ = np.sum(pos)
     if type_ == 'CRS' and sum_ == 1:
         return 0
@@ -28,7 +29,6 @@ def construct_cdhs_function(coeff1, coeff2, type_):
     """Create the CDHS function by substituting the exoplanet
     parameters. Type could be CRS or DRS.
     """
-
     penalty_ = functools.partial(penalty, type_=type_)
 
     def cdhpf(pos):
@@ -38,4 +38,9 @@ def construct_cdhs_function(coeff1, coeff2, type_):
 
 
 for row in exoplanets.iterrows():
-    print(row)
+    # Estimating CDHSi at CRS.
+    cdhsi_crs = construct_cdhs_function(coeff1=row['Radius'],
+                                        coeff2=row['Density'],
+                                        type_='CRS')
+    swarm = Swarm(npart=NPART, ndim=2, fitness_function=cdhsi_crs,
+                  learn_rate1=0.1, learn_rate2=0.1, max_velocity=1.0)
--- pso/pso.py
+++ pso/pso.py
@@ -1,47 +1,47 @@
-import numpy as np
+from numpy import abs as abs_
+from numpy.random import uniform
 
 
 class Particle:
+
     """A single particle of the swarm."""
 
-    def __init__(self, ndim, fitness_function, learn_rate1=2.0,
-                 learn_rate2=2.0, max_velocity=None):
-        self.curr_fitness = 0.0
-        self.best_fitness = 0.0
+    def __init__(self, ndim, fitness_function, min_=0, max_=1,
+                 learn_rate1=2.0, learn_rate2=2.0, max_velocity=None):
 
         self.fitness = lambda: fitness_function(self.position)
-        self.position = np.random.rand(ndim)
+        self.position = uniform(min_, max_, ndim)
+        self.velocity = uniform(-abs_(max_-min_), abs_(max_-min_), ndim)
 
-        self.velocity = 0
+        self.best = self.position
+        self.best_fitness = self.fitness()
         self.velocity_cap = max_velocity
 
         self.c1 = learn_rate1
         self.c2 = learn_rate2
 
-    def update(self):
+    def update(self, global_best):
         """Calculate velocity and update current position."""
+        dv_l = self.c1 * uniform(0, 1) * (self.best - self.position)
+        dv_g = self.c2 * uniform(0, 1) * (global_best - self.position)
 
-        dv_1 = self.c1 * np.random.rand() * (self.curr_fitness - self.position)
-        dv_2 = self.c2 * np.random.rand() * (self.best_fitness - self.position)
-        self.velocity += (dv_1 + dv_2)
+        self.velocity += dv_l + dv_g
         if self.velocity >= self.velocity_cap:
             self.velocity = self.velocity_cap
-        self.position += self.velocity
 
-        self.curr_fitness = self.fitness()
-        if self.curr_fitness > self.best_fitness:
-            self.best_fitness = self.curr_fitness
+        self.position += self.velocity
+        curr_fitness = self.fitness()
+        if curr_fitness > self.best_fitness:
+            self.best = self.position
+            self.best_fitness = curr_fitness
 
 
 class Swarm:
+
     """The swarm for PSO."""
 
-    def __init__(self, npart, ndim, fitness_function, learn_rate1=2.0,
-                 learn_rate2=2.0, max_velocity=None):
-        self.particles = [
-            Particle(ndim, fitness_function, learn_rate1, learn_rate2,
-                     max_velocity) for it in range(npart)
-        ]
+    def __init__(self, npart, *args, **kwargs):
+        self.particles = [Particle(*args, **kwargs) for it in range(npart)]
 
     def __iter__(self):
         """Iterate over the particles."""
@@ -60,12 +60,35 @@ class Swarm:
     @property
     def best_particle(self):
         """Returns the first particle found with global_best fitness."""
-        gbest = self.global_best
+        global_best = self.global_best              # caching for the loop.
         for p in self:
-            if p.best_fitness == gbest:
+            if p.best_fitness == global_best:
                 return p
 
     def update(self):
         """Update all particles of the swarm."""
         for p in self:
             p.update()
+
+    def converge(self, max_iter=10000, threshold=0.001, verbose=True):
+        """Find the maxima for the fitness function specified."""
+        for it in range(max_iter):
+            old_best = self.global_best
+            self.update()
+            if abs_(old_best - self.global_best) < threshold:
+                break
+            if verbose:
+                progress = round((it/max_iter) * 20)
+                print('Iterations: [',
+                      '=' * (progress-1),
+                      '>',
+                      ' ' * (20-progress),
+                      ']',
+                      end='\r', sep='')
+        if verbose:
+            print('Iterations: [',
+                  '=' * (progress-1),
+                  '#',
+                  ' ' * (20-progress),
+                  ']',
+                  end='\n', sep='')
